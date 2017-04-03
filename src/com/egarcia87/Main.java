15e9fca119dff8536d123281df956c11dcf7fa10
Erick Garcia <egarcia87@miners.utep.edu>
2017-04-02 22:57:36 -0600

    Changes for the lab to include both of the team members name

--- src/com/egarcia87/Main.java
+++ src/com/egarcia87/Main.java
@@ -4,31 +4,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		CreatePemKeys pemKeysGenerator = new CreatePemKeys();
-		//Comment these lines if you have already generated the pem files and want to test for the CAcertificate
-//		pemKeysGenerator.createPemKeys("files/Erick-GarciaClientSign");
-//		pemKeysGenerator.createPemKeys("files/Erick-GarciaClientEncrypt");
-//		pemKeysGenerator.createPemKeys("files/Erick-GarciaServerSign");
-//		pemKeysGenerator.createPemKeys("files/Erick-GarciaServerEncrypt");
+//		pemKeysGenerator.createPemKeys("files/Garcia_PalominoClientSign");
+//		pemKeysGenerator.createPemKeys("files/Garcia_PalominoClientEncrypt");
+//		pemKeysGenerator.createPemKeys("files/Garcia_PalominoServerSign");
+//		pemKeysGenerator.createPemKeys("files/Garcia_PalominoServerEncrypt");
 
 		Encrypt encryptor = new Encrypt();
 		Decrypt decryptor = new Decrypt();
 
-		System.out.println("Encryptor:");
-		encryptor.encrypt("files/Erick-GarciaClientSignPublic.pem", "Erick Garcia<egarcia87@miners.utep.edu>, 02/04/2017");
-		System.out.println("\nDecryptor:");
-		decryptor.decrypt("files/Erick-GarciaClientSignPrivate.pem", "encryptedMessage.txt");
+		System.out.println("Encryption:");
+		encryptor.encrypt("files/Garcia_PalominoClientEncryptPublic.pem", "Abner Palomino & Erick Garcia, 02/04/2017");
+		System.out.println("\nDecryption:");
+		decryptor.decrypt("files/Garcia_PalominoClientEncryptPrivate.pem", "encryptedMessage.txt");
 
 		Sign signer = new Sign();
 		Verify verifier = new Verify();
 
-		System.out.println("\nSign:");
-		signer.sign("files/Erick-GarciaClientSignPrivate.pem", "Erick Garcia<egarcia87@miners.utep.edu>, 02/04/2017");
-		System.out.println("\nVerify:");
-		verifier.verify("files/Erick-GarciaClientSignPublic.pem", "Whatevs");
-		verifier.verify("files/Erick-GarciaClientSignPublic.pem", "Erick Garcia<egarcia87@miners.utep.edu>, 02/04/2017");
+		System.out.println("\nSignature:");
+		signer.sign("files/Garcia_PalominoClientSignPrivate.pem", "Abner Palomino & Erick Garcia, 02/04/2017, 02/04/2017");
 
-		System.out.println("\nVerifying Certificate with Longpre client certificate:");
+		System.out.println("\nVerification:");
+		verifier.verify("files/Garcia_PalominoClientSignPublic.pem", "Sample Message that has had its contents altered");
+		verifier.verify("files/Garcia_PalominoClientSignPublic.pem", "Abner Palomino & Erick Garcia, 02/04/2017, 02/04/2017");
+
 		VerifyCert certificateVerifier = new VerifyCert();
+		System.out.println("\nVerifying Certificate with Longpre client certificate:");
 		certificateVerifier.verifyCertificate("files/CApublicKey.pem", "files/client1Certificate.txt");
 		System.out.println("\nVerifying Certificate with local client certificate:");
 		certificateVerifier.verifyCertificate("files/CApublicKey.pem", "files/certificate.txt");
